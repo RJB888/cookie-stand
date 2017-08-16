ccefd8bc405d83185556096721f9df446cdec418
Rob Bronson <robert.j.bronson@gmail.com>
2017-08-15 21:46:21 -0700

    Added styling.  Tried to get rows colored properly.. but footer doesn't seem to fit into the nthChild seledtor....

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ function Store(name, hours, minCust, maxCust, cookiePerHr) {
 
   this.customerPerHr = function() {
     var hourlyCustomerEst = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
-    //console.log('customers this hour: ' + hourlyCustomerEst);
     return hourlyCustomerEst;
   };
 
@@ -24,11 +23,8 @@ function Store(name, hours, minCust, maxCust, cookiePerHr) {
     this.hourlyCookies = [];
     this.dailyCookies = 0;
     for (var y = 0; y < this.storeHours.length; y++){
-      //console.log('Iteration: ' + y);
       this.hourlyCookies.push(Math.ceil(this.customerPerHr() * this.avgCookiePer));
-      //console.log('hourly cookies: ' + this.hourlyCookies[y]);
       this.dailyCookies += this.hourlyCookies[y];
-      //console.log('current cookie total ' + this.dailyCookies);
     }
   };
 
@@ -59,9 +55,6 @@ function Store(name, hours, minCust, maxCust, cookiePerHr) {
   this.calcAvgCookiePerHr();
 };
 
-// when you create the HTML element and store it into a variable - you shouldn't have to select that item again since it is stored in the variable already
-
-
 function generateReport(storesArray){
 
   var myTable = document.createElement('table');
@@ -90,7 +83,7 @@ function generateReport(storesArray){
   }
 
   function buildFoot(){
-    var myFooter = document.createElement('tfoot');
+    var myFooter = document.createElement('tr');
     myTable.appendChild(myFooter);
     var newCell = document.createElement('td');
     newCell.innerText = 'Total: ';
@@ -107,52 +100,20 @@ function generateReport(storesArray){
 
   buildHead();
   buildBody();
+
   for (var i = 0; i < storesArray.length; i++){
     storesArray[i].addYoSelf();
   }
+
   buildFoot();
 
 }
-var storeA = new Store('Kirk', storeOpenHours, 2, 20, 1.5);
-//debugger;
-var storeB = new Store('Spock', storeOpenHours, 30, 60, 2.5);
-//debugger;
-var storeC = new Store('Bones', storeOpenHours, 20, 80, 1);
-//debugger;
-var storeD = new Store('Chekov', storeOpenHours, 10, 60, 3.2);
-//debugger;
-var myLocations = [storeA, storeB, storeC, storeD];
+
+var firstPike = new Store('First and Pike', storeOpenHours, 23, 65, 6.3);
+var seaTac = new Store('SeaTac Airport', storeOpenHours, 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', storeOpenHours, 11, 38, 3.7);
+var capitolHill = new Store('Capitol Hill', storeOpenHours, 20, 38, 2.3);
+var alki = new Store('Alki', storeOpenHours, 2, 16, 4.6);
+var myLocations = [firstPike, seaTac, seattleCenter, capitolHill, alki];
 
 generateReport(myLocations);
-//
-//
-// function addLocation(name){
-//   var listAddition = document.createElement('ul');
-//   var tempElement = document.getElementById('salesBody');
-//   listAddition.setAttribute('id', name);
-//   tempElement.appendChild(listAddition);
-//   var titleLi = document.createElement('li');
-//   titleLi.innerText = name.toUpperCase();
-//   listAddition.appendChild(titleLi);
-//   for (var i = 6; i < 21; i++){
-//     var listItem = document.createElement('li');
-//     listItem.setAttribute('id', name + i);
-//     if (i < 12){
-//       listItem.innerText = i + '   a.m. :  ' + locations[q].hourlyCookies[i - 6] + ' cookies.';
-//       listAddition.appendChild(listItem);
-//     }
-//     else if (i === 12){
-//       listItem.innerText = i + ' p.m. : ' + locations[q].hourlyCookies[i - 6] + ' cookies.';
-//       listAddition.appendChild(listItem);
-//     }
-//     else {
-//       listItem.innerText = (i - 12) + '   p.m. :  ' + locations[q].hourlyCookies[i - 6] + ' cookies.';
-//       listAddition.appendChild(listItem);
-//     }
-//   }
-//   var totalElement = document.createElement('li');
-//   totalElement.setAttribute('id', name + 'Total');
-//   totalElement.innerText = name.toUpperCase() + ' Daily Total:  ' + locations[q].dailyCookies + ' cookies.';
-//   listAddition.appendChild(totalElement);
-// }
-//
